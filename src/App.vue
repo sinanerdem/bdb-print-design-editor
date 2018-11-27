c9858ba856118675c2f18a2b26bdf5265c51114b
Sinan Erdem <erdesina@amazon.co.uk>
2018-11-27 16:35:42 +0000

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <canvas-area/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CanvasArea from './components/CanvasArea.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CanvasArea
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CanvasArea.vue
@@ -0,0 +1,28 @@
+<template>
+  <canvas id="drawing">
+  </canvas>
+</template>
+
+<script>
+import { fabric } from 'fabric'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted: function () {
+    var canvas = new fabric.Canvas('drawing', {
+      width: 300,
+      height: 300,
+      isDrawingMode: false,
+      backgroundColor: '#fff'
+    });
+    canvas.add(new fabric.Rect({left: 0, top: 0, fill: '#000', width: 100, height: 100}));
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
